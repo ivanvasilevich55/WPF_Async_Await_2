81cdf4236f3d8407747a17119b1f2736a6507fc0
Ivan <ivan@yandex.ru>
2019-01-11 11:44:15 +0300

    чуть понятно про await await - отправляет task.run() и не выполняется дальше! Когда task.run() закончился - началось дальше выполнения до следующего await task.run() await'ов может быть бесконечно. часть кода между await - callback

--- ConsoleAppAsyncAwait/Module1.vb
+++ ConsoleAppAsyncAwait/Module1.vb
@@ -1,7 +1,43 @@
-﻿Module Module1
+﻿Imports System.Threading
+Imports System.Threading.Tasks
+
+
+Module Module1
 
 	Sub Main()
+		Console.WriteLine("Start main()")
+		SomeMethodAsync() 'в данном методе есть await'ы ( и после await'ов есть синхронный код, который будет выполнятся, как колбэк т.е. после очередного awaita
+		Console.WriteLine("await from someMethod")
+		Console.ReadLine()
+	End Sub
+
+
+
+
+	Sub MyMethodAsync()
+		Console.WriteLine("Start MyMethod() withaout await")
+		Thread.Sleep(8000)
+		Console.WriteLine("Выполняется долгая процедура1")
+	End Sub
+
+
+
+
+	Async Sub SomeMethodAsync() ' async означает, что дойдя до awaita - управление передатся вызывающему потоку
+		Console.WriteLine("Start SomeMethod()")
+		Await Task.Run(Sub()
+						   MyMethodAsync()
+					   End Sub)
+		Console.WriteLine("END SomeMethod()") ' это как бы колбэк - будет выполняться Только после завершения await!!!
+
+
+		Await Task.Run(Sub()
+						   MyMethodAsync()
+					   End Sub)
+		Console.WriteLine("Конец 2 await")
 
 	End Sub
+
+
 
 End Module
