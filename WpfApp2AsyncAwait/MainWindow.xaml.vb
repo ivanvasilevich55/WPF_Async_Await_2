3ca1a3cf279cd5c1605cc91b0647afa51f44055f
Ivan <ivan@yandex.ru>
2019-01-11 12:11:40 +0300

    await - WPF следующая часть кода - callback

--- WpfApp2AsyncAwait/MainWindow.xaml.vb
+++ WpfApp2AsyncAwait/MainWindow.xaml.vb
@@ -1,3 +1,19 @@
-﻿Class MainWindow
-
+﻿Imports System
+Imports System.Threading
+Imports System.Threading.Tasks
+'async-await - синтаксический сахар (
+Class MainWindow
+	Private Sub mysub()
+		Thread.Sleep(5000)
+		MsgBox("END")
+	End Sub
+	Private Async Sub mysubAsync()
+		Await Task.Run(Sub() Thread.Sleep(5000))
+		MsgBox("END1")
+		Await Task.Run(Sub() Thread.Sleep(5000))
+		MsgBox("END2")
+	End Sub
+	Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+		mysubAsync()
+	End Sub
 End Class
